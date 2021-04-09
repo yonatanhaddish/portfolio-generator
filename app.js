5c7dd8d7951daf41fbd80eb54ba1ae9a76695bd4
Your Name <you@example.com>
2021-04-09 16:54:40 -0400

    node first lesson

---
+++ app.js
@@ -0,0 +1,12 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+const printProfileData= profileDataArr => {
+    for (let i=0; i<profileDataArr.length; i++) {
+        console.log(profileDataArr[i]);
+    }
+    
+};
+
+printProfileData(profileDataArgs);
+
