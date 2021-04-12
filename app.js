e590514a4c61acd40b60cfa8c1f2afc3c408d2af
Your Name <you@example.com>
2021-04-12 16:49:01 -0400

    first trial

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs= require('fs');
+const generatePage= require('./src/page-template')
+const profileDataArgs = process.argv.slice(2);
+ const [name, github]= profileDataArgs;
 // console.log(profileDataArgs);
 
-const printProfileData= profileDataArr => {
-    for (let i=0; i<profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+// const printProfileData= profileDataArr => {
+//     for (let i=0; i<profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
     
-};
+// };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+fs.writeFile('index.html',generatePage(name, github), err => {
+    if (err) throw err;
+    console.log("Portfolio complete! Checkout index.html to see the output!")
+});
 
