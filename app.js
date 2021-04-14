d85848422fa95969c98080a56c0a0940a4c377c7
Your Name <you@example.com>
2021-04-14 16:53:15 -0400

    inquirer

--- app.js
+++ app.js
@@ -1,23 +1,37 @@
-const fs= require('fs');
-const generatePage= require('./src/page-template')
-const profileDataArgs = process.argv.slice(2);
- const [name, github]= profileDataArgs;
-// console.log(profileDataArgs);
+const inquirer= require('inquirer');
+// const fs= require('fs');
+// const generatePage= require('./src/page-template')
 
-// const printProfileData= profileDataArr => {
-//     for (let i=0; i<profileDataArr.length; i++) {
-//         console.log(profileDataArr[i]);
+// const pageHTML= generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
+//     console.log("Portfolio complete! Checkout index.html to see the output!")
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+
+// ar inquirer = require('inquirer');
+// inquirer
+//   .prompt([
+//     /* Pass your questions in here */
+//   ])
+//   .then(answers => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch(error => {
+//     if(error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else went wrong
 //     }
-    
-// };
-
-// printProfileData(profileDataArgs);
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-fs.writeFile('index.html',generatePage(name, github), err => {
-    if (err) throw err;
-    console.log("Portfolio complete! Checkout index.html to see the output!")
-});
+//   });
 
